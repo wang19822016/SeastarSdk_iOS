88471d5a376e4b87ee22e120290de8e0d170deef
w <wang19822016@outlook.com>
2016-10-31 14:42:08 +0800

    添加键盘遮挡和点击空白处关闭功能

--- SeastarSdk/BaseViewController.swift
+++ SeastarSdk/BaseViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class BaseViewController: UIViewController {
+    
+    private var isUped: Bool = false
 
     override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
         super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
@@ -52,6 +54,48 @@ class BaseViewController: UIViewController {
     func makeBounds(_ layer: CALayer) {
         layer.cornerRadius = 4;
         layer.masksToBounds = true;
+    }
+    
+    func moveUp(_ frame: CGRect) {
+        let keyboardHeight: CGFloat = 216.0 + 35.0 //键盘高度216 键盘上tabbar35
+        let keyboardY: CGFloat = self.view.frame.height - keyboardHeight
+        let viewDownEdgeY = frame.origin.x + frame.size.height
+        let offset = viewDownEdgeY - keyboardY
+        UIView.beginAnimations("Resize", context: nil)
+        UIView.setAnimationDuration(0.30)
+        let width = self.view.frame.size.width
+        let height = self.view.frame.size.height
+        if offset > 0 {
+            let rect = CGRect(x: 0, y: -offset, width: width, height: height)
+            self.view.frame = rect
+        }
+        UIView.commitAnimations()
+        
+        isUped = true
+    }
+    
+    // 传入要移动控件的frame
+    func moveDown() {
+        UIView.beginAnimations("resize", context: nil)
+        UIView.setAnimationDuration(0.30)
+        let rect = CGRect(x: 0, y: 0, width: self.view.frame.size.width, height: self.view.frame.size.height)
+        self.view.frame = rect
+        UIView.commitAnimations()
+        
+        isUped = false
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if isUped {
+            self.view.endEditing(true)
+            UIView.beginAnimations("resize", context: nil)
+            UIView.setAnimationDuration(0.30)
+            let rect = CGRect(x: 0, y: 0, width: self.view.frame.size.width, height: self.view.frame.size.height)
+            self.view.frame = rect
+            UIView.commitAnimations()
+        }
+        
+        isUped = false
     }
 }
 
