29b63220696acad3c492ca3fcc4284f09512c33c
wujie1416 <18911578088@163.com>
2016-10-31 10:28:57 +0800

    竖屏

---
+++ SeastarSdk/ForgetPortraitViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ForgetPortraitViewController.swift
+//  SeastarSdk_iOS
+//
+//  Created by seastar on 16/10/31.
+//
+//
+
+import UIKit
+
+class ForgetPortraitViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
